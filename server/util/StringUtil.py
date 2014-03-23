262bdeeca036d8ea722e1a758488f15c96d790c3
Wang Hao <eric.3126@hotmail.com>
2014-03-23 16:57:00 +0800

    Add active_code_generator

--- server/util/StringUtil.py
+++ server/util/StringUtil.py
@@ -8,3 +8,6 @@ import hashlib
 def token_generator():
 	code = str(time.time()) + str(random.randint(1, 100000))
 	return hashlib.md5(code).hexdigest()
+
+def active_code_generator():
+	return str(random.randint(10000,99999))
